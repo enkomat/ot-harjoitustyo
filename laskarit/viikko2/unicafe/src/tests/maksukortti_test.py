b1009a34a27cbca330a8a277fb7ff83b99d8e23f
Matias Enkovaara <matias.enkovaara@helsinki.fi>
2021-03-30 15:23:28 +0300

    added all required tests for /unicafe/maksukortti.py

--- laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
+++ laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
@@ -7,3 +7,17 @@ class TestMaksukortti(unittest.TestCase):
 
     def test_luotu_kortti_on_olemassa(self):
         self.assertNotEqual(self.maksukortti, None)
+    
+    def test_saldo_alussa_oikein(self):
+        self.assertEqual(str(self.maksukortti), "saldo: 0.1")
+    
+    def test_rahan_lataaminen_kortille_kasvattaa_saldoa_oikein(self):
+        self.maksukortti.lataa_rahaa(100)
+        self.assertEqual(str(self.maksukortti), "saldo: 1.1")
+
+    def test_rahan_ottaminen_kortilta_toimii(self):
+        self.maksukortti.lataa_rahaa(1000)
+        self.maksukortti.ota_rahaa(44)
+        self.assertEqual(str(self.maksukortti), "saldo: 9.66")
+        self.maksukortti.ota_rahaa(7500)
+        self.assertEqual(str(self.maksukortti), "saldo: 9.66")
